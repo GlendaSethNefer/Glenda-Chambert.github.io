1d632f63fc5453f7c2a7d726b8f0ee22c6ae14b5
GlendaSethNefer <glendamerica@gmail.com>
2023-06-29 00:59:01 -0400

    botones con js no funcionan

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,13 @@ module.exports = {
   darkMode: 'false', //or 'media' or 'class'
   content: [
     "./**/*.{html,js}",
-    "/assets/publico/.{html.js}",
+    "./assets/publico/*.{html,js}",
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
+  
   theme: {
-    extend: {},
-    screens: {
-      'movil': {'min': '375px', 'max': '768'},
-    },
+    extend: {}
   },
-    plugins: [require("tw-elements/dist/plugin.cjs")]
+  
+  plugins: [require("tw-elements/dist/plugin.cjs")]
   }
